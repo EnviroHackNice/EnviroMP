7b2d99616a43eb1905751ccea641cd408f1d4847
pixelzery <pixelzery@gmail.com>
2021-03-13 20:08:42 +0000

    begin link frontend

--- main.py
+++ main.py
@@ -10,47 +10,50 @@ API_KEY = "C6FuYkASnMvvCM4hJaBoukGK"
 
 app = flask.Flask(__name__)
 
-mp_data = json.loads(open("mps_cache.json", "r"))
-party_data = json.loads(open("parties.json", "r"))
+with open("mps_cache.json", "r") as f:
+	mp_data = json.load(f)
+
+with open("parties.json", "r") as f:
+	party_data = json.load(f)
 
 def refetch_mp_data():
-    mp_fetch()
-    party_fetch()
-    mp_data = json.loads(open("mps_cache.json", "r"))
-    party_data = json.loads(open("parties.json", "r"))
-    return (mp_data, party_data)
+	mp_fetch()
+	party_fetch()
+	mp_data = json.load(open("mps_cache.json", "r"))
+	party_data = json.load(open("parties.json", "r"))
+	return (mp_data, party_data)
 
 @app.context_processor
 def inject_site_data():
-    return {
-        "site": {
-            "title": "EnviroMP",
-            "tagline": "Score MPs’ environmental-friendliness",
-            # "url": "",
-            "version": "0.1.0",
-            # "google_analytics_id": "",
-            "year": datetime.now().year,
-        }
-    }
+	return {
+		"site": {
+			"title": "EnviroMP",
+			"tagline": "Score MPs’ environmental-friendliness",
+			# "url": "",
+			"version": "0.1.0",
+			# "google_analytics_id": "",
+			"year": datetime.now().year,
+		}
+	}
 
 @app.route("/mp-search")
 def mp_search():
-    return flask.render_template(r"pages/mp-search.html", title="MP Search")
+	return flask.render_template(r"pages/mp-search.html", title="MP Search")
 
 @app.route("/mp-compare")
 def mp_compare():
-    return flask.render_template(r"pages/mp-compare.html", title="MP Compare")
+	return flask.render_template(r"pages/mp-compare.html", title="MP Compare")
 
 @app.route('/res/<path:path>')
 def send_res(path):
-    return send_from_directory('res', path)
+	return flask.send_from_directory('res', path)
 
 # todo
 @app.route("/api/get_mp/<mpn:str>", methods=["GET"])
 def api_get_mp(mpn):
-    for mp in mp_data:
-        if mp["mpn"] == mpn:
-            return json.dumps(mp)
+	for mp in mp_data:
+		if mp["mpn"] == mpn:
+			return json.dumps(mp)
 
 @app.route("/api/get_parties", methods=["GET"])
 def api_get_parties():
@@ -60,5 +63,5 @@ def api_get_parties():
 # todo
 @app.route("/api/get_mps", methods=["GET"])
 def api_get_mps():
-    serialized = json.dumps(mp_data)
-    return serialized
+	serialized = json.dumps(mp_data)
+	return serialized
